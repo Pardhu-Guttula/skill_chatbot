49d5d663e5924a0d1be31a752bd985798c2637d1
Ranga19 <gaddamranganath32@gmail.com>
2023-12-02 17:02:21 +0530

    model modified

---
+++ main/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class UserInputForm(forms.Form):
+    user_input = forms.CharField(widget=forms.Textarea)
---
+++ main/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Conversation(models.Model):
+    user_input = models.TextField()
+    model_response = models.TextField()
