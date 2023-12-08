27603812f9c16c0466939a2591821dc1b7c4225a
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2023-12-08 23:32:28 +0530

    updated ui

--- main/forms.py
+++ main/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class UserInputForm(forms.Form):
-    user_input = forms.CharField(widget=forms.TextInput(attrs={"rows": 2, "cols": 47}))
+    user_input = forms.CharField(widget=forms.TextInput())
